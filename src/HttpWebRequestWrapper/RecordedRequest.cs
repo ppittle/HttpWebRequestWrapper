78bcec4af3bcb96480a2afceea9d9744d3ad90f9
Philip Pittle <philip.pittle@skykick.com>
2018-02-10 01:13:39 -0800

    cleaned up usings

--- src/HttpWebRequestWrapper/RecordedRequest.cs
+++ src/HttpWebRequestWrapper/RecordedRequest.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
-using System.Runtime.CompilerServices;
 
 namespace HttpWebRequestWrapper
 {
