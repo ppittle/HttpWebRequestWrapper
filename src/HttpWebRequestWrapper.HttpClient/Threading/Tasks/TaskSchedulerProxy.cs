a2c8b3de852180c778d7935cfa5e5dd7e9030cd6
Philip Pittle <philip.pittle@skykick.com>
2018-02-28 19:26:42 -0800

    redesign of httpclient integration - http client is fully supported (for the most part) and no longer needs a custom Http Message Handler

---
+++ src/HttpWebRequestWrapper.HttpClient/Threading/Tasks/TaskSchedulerProxy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HttpWebRequestWrapper.HttpClient.Extensions;
+
+namespace HttpWebRequestWrapper.HttpClient.Threading.Tasks
+{
+    /// <summary>
+    /// Black Magic.  Intercepts the scheduling of all Tasks
+    /// so the passed <see cref="IVisitTaskOnSchedulerQueue"/> can 
+    /// inspect them and potentially modify them, like 
+    /// changing thier <see cref="Task.AsyncState"/>.
+    /// <para />
+    /// <see cref="HttpClientHandlerStartRequestTaskVisitor"/> for an 
+    /// example and more informaton.
+    /// </summary>
+    internal class TaskSchedulerProxy : TaskScheduler
+    {
+        private readonly IVisitTaskOnSchedulerQueue _taskVisitor;
+        private readonly TaskScheduler _inner;
+
+        public TaskSchedulerProxy(IVisitTaskOnSchedulerQueue taskVisitor, TaskScheduler inner)
+        {
+            _taskVisitor = taskVisitor;
+            _inner = inner;
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            return _inner.GetScheduledTasks();
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            _taskVisitor.Visit(task);   
+
+            _inner.QueueTask(task);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            return _inner.TryExecuteTaskInline(task, taskWasPreviouslyQueued);
+        }
+    }
+}
--- src/HttpWebRequestWrapper.Tests/HttpClientTests.cs
+++ src/HttpWebRequestWrapper.Tests/HttpClientTests.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using HttpWebRequestWrapper.HttpClient;
 using Should;
 using Xunit;
+
+// Justification: Test class
+// ReSharper disable InconsistentNaming
 
 namespace HttpWebRequestWrapper.Tests
 {
@@ -28,9 +30,9 @@ namespace HttpWebRequestWrapper.Tests
             HttpResponseMessage response;
 
             // ACT
-            using (new HttpWebRequestWrapperSession(new HttpWebRequestWrapperRecorderCreator(recordingSession)))
+            using (new HttpClientAndRequestWrapperSession(new HttpWebRequestWrapperRecorderCreator(recordingSession)))
             {
-                var httpClient = new System.Net.Http.HttpClient(new HttpClientHandlerInterceptor());
+                var httpClient = new System.Net.Http.HttpClient();
 
                 response = await httpClient.GetAsync(url);
             }
@@ -45,6 +47,33 @@ namespace HttpWebRequestWrapper.Tests
             recordingSession.RecordedRequests[0].ResponseBody.ShouldContain("<html");
         }
 
+        // WARNING!! Makes live request
+        [Fact]
+        public async Task CanRecordWebRequestException()
+        {
+            // ARRANGE
+            var recordingSession = new RecordingSession();
+            HttpResponseMessage response;
+
+            // ACT
+            using (new HttpClientAndRequestWrapperSession(new HttpWebRequestWrapperRecorderCreator(recordingSession)))
+            {
+                var httpClient = new System.Net.Http.HttpClient();
+
+                response = await httpClient.GetAsync("https://accounts.google.com/o/oauth2/auth");
+            }
+
+            // ASSERT
+            response.ShouldNotBeNull();
+
+            recordingSession.RecordedRequests.ShouldNotBeEmpty();
+            recordingSession.RecordedRequests[0].ResponseStatusCode.ShouldEqual(HttpStatusCode.BadRequest);
+            
+            // HttpClient suppresses exceptions - so response will just be in ResponseBody
+            recordingSession.RecordedRequests[0].ResponseBody.ShouldContain("<html");
+            recordingSession.RecordedRequests[0].ResponseException.ShouldBeNull();
+        }
+
         [Fact]
         public async Task CanInterceptAndSpoofResponse()
         {
@@ -57,9 +86,9 @@ namespace HttpWebRequestWrapper.Tests
             HttpResponseMessage response;
 
             // ACT
-            using (new HttpWebRequestWrapperSession(new HttpWebRequestWrapperInterceptorCreator(responseCreator)))
+            using (new HttpClientAndRequestWrapperSession(new HttpWebRequestWrapperInterceptorCreator(responseCreator)))
             {
-                var httpClient = new System.Net.Http.HttpClient(new HttpClientHandlerInterceptor());
+                var httpClient = new System.Net.Http.HttpClient();
 
                 response = await httpClient.GetAsync(new Uri("http://fakeSite.fake"));
             }
@@ -72,6 +101,52 @@ namespace HttpWebRequestWrapper.Tests
         }
 
         [Fact]
+        public async Task CanSupportMultipleConcurrentHttpClients()
+        {
+            // ARRANGE
+            var url1 = new Uri("http://fakeSite.fake/1");
+            var url2 = new Uri("http://fakeSite.fake/2");
+            var url3 = new Uri("http://fakeSite.fake/3");
+
+            var response1 = "response1";
+            var response2 = "response2";
+            var response3 = "response3";
+
+            var responseCreator = new Func<InterceptedRequest, HttpWebResponse>(req =>
+            {
+                if (req.HttpWebRequest.RequestUri == url1)
+                    return req.HttpWebResponseCreator.Create(response1);
+
+                if (req.HttpWebRequest.RequestUri == url2)
+                    return req.HttpWebResponseCreator.Create(response2);
+
+                if (req.HttpWebRequest.RequestUri == url3)
+                    return req.HttpWebResponseCreator.Create(response3);
+
+                throw new Exception($"Couldn't match url [{req.HttpWebRequest.RequestUri}]");
+            });
+
+            using (new HttpClientAndRequestWrapperSession(new HttpWebRequestWrapperInterceptorCreator(responseCreator)))
+            {
+                // ACT
+                var sharedClient = new System.Net.Http.HttpClient();
+
+                var task1 = sharedClient.GetStringAsync(url1);
+                var task2 = sharedClient.GetStringAsync(url2);
+                var task3 = new System.Net.Http.HttpClient().GetStringAsync(url3);
+                var task1b = new System.Net.Http.HttpClient().GetStringAsync(url1);
+
+                await Task.WhenAll(task1, task2, task3, task1b);
+
+                // ASSERT
+                task1.Result.ShouldEqual(response1);
+                task2.Result.ShouldEqual(response2);
+                task3.Result.ShouldEqual(response3);
+                task1b.Result.ShouldEqual(response1);
+            }
+        }
+
+        [Fact]
         public async Task CanInterceptAndSpoofWebRequestException()
         {
             // ARRANGE
@@ -80,18 +155,17 @@ namespace HttpWebRequestWrapper.Tests
             var statusCode = HttpStatusCode.Forbidden;
 
             var responseCreator = new Func<InterceptedRequest, HttpWebResponse>(req =>
-                throw new WebException(
-                    "test exception",
-                    innerException: null,
-                    status: WebExceptionStatus.SendFailure,
-                    response: req.HttpWebResponseCreator.Create(responseBody, statusCode)));
+                // http web client exceptions won't be recorded as exceptions, just a normal response
+                    req.HttpWebResponseCreator.Create(
+                        responseBody,
+                        statusCode));
             
             HttpResponseMessage response;
 
             // ACT
-            using (new HttpWebRequestWrapperSession(new HttpWebRequestWrapperInterceptorCreator(responseCreator)))
+            using (new HttpClientAndRequestWrapperSession(new HttpWebRequestWrapperInterceptorCreator(responseCreator)))
             {
-                var httpClient = new System.Net.Http.HttpClient(new HttpClientHandlerInterceptor());
+                var httpClient = new System.Net.Http.HttpClient();
 
                 response = await httpClient.GetAsync(new Uri("http://fakeSite.fake"));
             }
